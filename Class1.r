d1c61251deec838adcb8f2523c671b20f9722652
Tella <TELLAA02@pfizer.com>
2025-02-24 13:50:53 -0500

    ALL

---
+++ Class1.r
@@ -0,0 +1,15 @@
+# Load the haven package
+library(haven)
+
+# Load the sas7bdat package
+library(sas7bdat)
+
+<<<<<<< HEAD
+#FIlter the data set    
+data <- read_sas("C:/Users/Class1.sas7bdat")
+data <- data %>% filter(Year == 2018)
+=======
+#Testing Git Fetch 
+
+    
+>>>>>>> 8b1040167a50eff713ec901856f8953fa522a29f
